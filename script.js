9691e48733a8a3a7715eefe189a4e60fea240f5f
ferniemann <hello@ferdinand-niemann.eu>
2022-06-10 15:18:08 +0200

    add: adds basic app functionalities with state

---
+++ script.js
@@ -0,0 +1,56 @@
+const btnAdd = document.querySelector("#add-todo");
+const state = {
+    todos: [
+        { description: "Live Session wuppen", done: false },
+        { description: "Wochenende", done: true },
+    ],
+};
+
+renderTodos();
+
+function renderTodos() {
+    const list = document.querySelector("#list");
+    list.innerText = "";
+
+    state.todos.forEach((todo) => {
+        const listEl = document.createElement("li");
+
+        const checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+
+        checkbox.addEventListener("change", (event) => {
+            changeTodoState(event, todo);
+        });
+
+        const text = document.createElement("p");
+        text.innerText = todo.description;
+
+        listEl.append(checkbox, text);
+        list.append(listEl);
+    });
+}
+
+function changeTodoState(e, el) {
+    const todoDoneState = e.target.checked;
+    el.done = todoDoneState;
+}
+
+btnAdd.addEventListener("click", addTodo);
+
+function addTodo() {
+    const todoText = document.querySelector("#todo-description");
+    const description = todoText.value;
+
+    const todo = new Todo(description);
+
+    state.todos.push(todo);
+
+    renderTodos();
+    todoText.value = "";
+}
+
+class Todo {
+    constructor(description) {
+        (this.description = description), (this.done = false);
+    }
+}
